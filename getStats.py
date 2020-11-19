50631808914eb160e822f036d455f93e0793ecce
Thomas <thomas@docker-001.thomaswimprine.local>
2020-11-19 12:31:53 -0600

    All states seperated by date

--- getStats.py
+++ getStats.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import datetime as dt
-from datetime import timedelta
+from datetime import timedelta, datetime
+import os
 
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -15,59 +16,86 @@ LOCAL_JSON_FILE = 'covid-19-cases.json'
 
 # Today's Date
 TODAY = pd.to_datetime("today")
+currentDir = os.getcwd()
+
+reportDir = os.path.join(currentDir, TODAY.strftime("%Y%m%d"))
+os.makedirs(reportDir, exist_ok = True)
+
+print(reportDir)
 
 # This is the GitHub URL for the Covid Tracking Project
 data_loc = ('https://api.covidtracking.com/v1/states/daily.json')
+states_url = ('https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json')
+
+states_response = requests.get(states_url)
+states_data = states_response.json()
+states_df = pd.DataFrame(states_data)
+
 
 # Read in the data to a pandas DataFrame.
 response = requests.get(data_loc)
 data = response.json()
 df = pd.DataFrame(data)
 
+#df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+
+
 df.insert(df.shape[-1],'fmtDate',pd.to_datetime(df.date, format='%Y%m%d'))
 #df['fmtDate'] = pd.to_datetime(df.date, format='%Y%m%d')
+#df = df[df.date != 6]
 
 df = df.iloc[::-1]
 
-louisiana = df.loc[df['state'] == 'LA']
-date = louisiana['fmtDate']
-deathIncrease = louisiana['deathIncrease']
-positiveIncrease = louisiana['positiveIncrease']
 
-fig = plt.figure(figsize=(100,40))
+for index, row in states_df.iterrows():
+    print(row['name'],row['abbreviation'])
+    
+    state = df.loc[df['state'] == row['abbreviation']]
+    
+    deathIncrease = state['deathIncrease']
+    positiveIncrease = state['positiveIncrease']
+    
+    fig = plt.figure(figsize=(100,40))
 
-louisiana.insert(louisiana.shape[-1],'SMA_3_deathIncrease', louisiana.loc[:,'deathIncrease'].rolling(window=3).mean())
-louisiana.insert(louisiana.shape[-1],'SMA_7_deathIncrease', louisiana.loc[:,'deathIncrease'].rolling(window=7).mean())
-louisiana.insert(louisiana.shape[-1],'SMA_28_deathIncrease', louisiana.loc[:,'deathIncrease'].rolling(window=28).mean())
-louisiana.insert(louisiana.shape[-1],'SMA_90_deathIncrease', louisiana.loc[:,'deathIncrease'].rolling(window=90).mean())
+    state.insert(state.shape[-1],'SMA_3_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=3).mean())
+    state.insert(state.shape[-1],'SMA_7_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=7).mean())
+    state.insert(state.shape[-1],'SMA_28_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=28).mean())
+    state.insert(state.shape[-1],'SMA_90_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=90).mean())
 
 
-louisiana.insert(louisiana.shape[-1],'SMA_3_positiveIncrease', louisiana.loc[:,'positiveIncrease'].rolling(window=3).mean())
-louisiana.insert(louisiana.shape[-1],'SMA_7_positiveIncrease', louisiana.loc[:,'positiveIncrease'].rolling(window=7).mean())
-louisiana.insert(louisiana.shape[-1],'SMA_28_positiveIncrease', louisiana.loc[:,'positiveIncrease'].rolling(window=28).mean())
-louisiana.insert(louisiana.shape[-1],'SMA_90_positiveIncrease', louisiana.loc[:,'positiveIncrease'].rolling(window=90).mean())
+    state.insert(state.shape[-1],'SMA_3_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=3).mean())
+    state.insert(state.shape[-1],'SMA_7_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=7).mean())
+    state.insert(state.shape[-1],'SMA_28_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=28).mean())
+    state.insert(state.shape[-1],'SMA_90_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=90).mean())
 
-plt.plot_date(date, louisiana['deathIncrease'], linestyle='solid', color= 'black', label='Louisiana Daily Increase in Covid Deaths')
-plt.plot_date(date, louisiana['SMA_3_deathIncrease'], color='red', linestyle='solid', label='3 day rolling average')
-plt.plot_date(date, louisiana['SMA_7_deathIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
-plt.plot_date(date, louisiana['SMA_28_deathIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
-plt.plot_date(date, louisiana['SMA_90_deathIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
-plt.grid(b=True, which='both', axis='both')
-plt.legend(loc='upper left')
-plt.savefig('CovidGraphDeaths' + time.strftime("%Y%m%d") + '.pdf', dpi=300 )
-plt.show()
+    labelText = row['name'] + "Daily Increase in Covid Deaths - " + time.strftime("%Y%m%d_%H:%M")
+    plt.plot_date(state['fmtDate'], state['deathIncrease'], linestyle='solid', color= 'black', label=labelText)
+    plt.plot_date(state['fmtDate'], state['SMA_3_deathIncrease'], color='red', linestyle='solid', label='3 day rolling average')
+    plt.plot_date(state['fmtDate'], state['SMA_7_deathIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
+    plt.plot_date(state['fmtDate'], state['SMA_28_deathIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
+    plt.plot_date(state['fmtDate'], state['SMA_90_deathIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
+    plt.grid(b=True, which='both', axis='both')
+    plt.legend(loc='upper left')
+    fileName = row['name'].replace(" ","_") + '_CovidGraphDeaths' + time.strftime("%Y%m%d") + '.pdf'
+    savePath = os.path.join(reportDir, fileName)
+    plt.savefig(savePath, dpi=75 )
+    #plt.show()
+    plt.close()
 
-fig = plt.figure(figsize=(100,40))
-plt.plot_date(date, positiveIncrease, linestyle='solid', label='Louisiana Daily Increase in Covid Positive Cases')
+    fig = plt.figure(figsize=(100,40))
+    labelText = row['name'] + "Daily Increase in Covid Positive Cases - " + time.strftime("%Y%m%d_%H:%M")
+    plt.plot_date(state['fmtDate'], positiveIncrease, linestyle='solid', label=labelText)
 
-plt.plot_date(date, louisiana['positiveIncrease'], color='black', linestyle='solid', label='Louisiana Daily Increase in Covid Positive Cases')
-plt.plot_date(date, louisiana['SMA_3_positiveIncrease'], color='red', linestyle='solid', label='3 day rolling average')
-plt.plot_date(date, louisiana['SMA_7_positiveIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
-plt.plot_date(date, louisiana['SMA_28_positiveIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
-plt.plot_date(date, louisiana['SMA_90_positiveIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
-plt.grid(b=True, which='both', axis='both')
-plt.legend(loc='upper left')
-plt.savefig('CovidGraphPositives' + time.strftime("%Y%m%d") + '.pdf', dpi=300 )
-plt.show()
-
+    plt.plot_date(state['fmtDate'], state['positiveIncrease'], color='black', linestyle='solid', label='state Daily Increase in Covid Positive Cases')
+    plt.plot_date(state['fmtDate'], state['SMA_3_positiveIncrease'], color='red', linestyle='solid', label='3 day rolling average')
+    plt.plot_date(state['fmtDate'], state['SMA_7_positiveIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
+    plt.plot_date(state['fmtDate'], state['SMA_28_positiveIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
+    plt.plot_date(state['fmtDate'], state['SMA_90_positiveIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
+    plt.grid(b=True, which='both', axis='both')
+    plt.legend(loc='upper left')
+    fileName = row['name'].replace(" ","_") + '_CovidGraphPositives' + time.strftime("%Y%m%d") + '.pdf'
+    savePath = os.path.join(reportDir, fileName)
+    plt.savefig(savePath, dpi=75 )
+    #plt.show()
+    plt.close()
 
