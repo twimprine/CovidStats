8feed0a3016321d408908b3cbd79c8e478d738d2
Thomas Wimprine <thomas@thomaswimprine.com>
2020-11-19 17:24:32 -0600

    Updated to only checkin new reports

--- cronScript.sh
+++ cronScript.sh
@@ -2,6 +2,6 @@
 
 DATE=`date +"%Y%m%d %H:%M"`
 cd $HOME/Repositories/CovidStats && $HOME/Repositories/CovidStats/getStats.py 
-git add .
+git add ./reports/*
 git commit -m "Hourly Update - ${DATE}"
 git push
