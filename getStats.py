11df328b77efb412141f49a1489d50e981f11bcc
Thomas Wimprine <thomas@thomaswimprine.com>
2020-12-10 17:07:33 -0600

    Cleaned up

--- getStats.py
+++ getStats.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import sys, json, requests, time
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,14 +5,8 @@ from matplotlib.ticker import MaxNLocator
 import datetime as dt
 from datetime import timedelta, datetime
 import os
+import numpy as np
 
-from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
-
-# If you have saved a local copy of the file
-# set READ_FROM_URL to True
-READ_FROM_URL = True
-LOCAL_JSON_FILE = 'covid-19-cases.json'
 
 # Today's Date
 TODAY = pd.to_datetime("today")
@@ -23,40 +15,58 @@ currentDir = os.getcwd()
 reportDir = os.path.join(currentDir,"reports",  TODAY.strftime("%Y%m%d"))
 os.makedirs(reportDir, exist_ok = True)
 
-print(reportDir)
 
-# This is the GitHub URL for the Covid Tracking Project
-data_loc = ('https://api.covidtracking.com/v1/states/daily.json')
+covid_data_loc = ('https://api.covidtracking.com/v1/states/daily.json')
+
+covid_data_response = requests.get(covid_data_loc)
+covid_data = covid_data_response.json()
+covid_df = pd.DataFrame(covid_data)
+
 states_url = ('https://raw.githubusercontent.com/twimprine/Datasets/main/states.json')
-#states_url = ('https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json')
-
 states_response = requests.get(states_url)
 states_data = states_response.json()
 states_df = pd.DataFrame(states_data)
 
 
-# Read in the data to a pandas DataFrame.
-response = requests.get(data_loc)
-data = response.json()
-df = pd.DataFrame(data)
+covid_df = covid_df.drop(['positive', 'probableCases', 'negative', 'pending',
+       'totalTestResultsSource', 'totalTestResults', 'hospitalizedCurrently',
+       'hospitalizedCumulative', 'inIcuCumulative',
+       'onVentilatorCumulative', 'recovered',
+       'dataQualityGrade', 'lastUpdateEt', 'dateModified', 'checkTimeEt',
+       'death', 'hospitalized', 'dateChecked', 'totalTestsViral',
+       'positiveTestsViral', 'negativeTestsViral', 'positiveCasesViral',
+       'deathConfirmed', 'deathProbable', 'totalTestEncountersViral',
+       'totalTestsPeopleViral', 'totalTestsAntibody', 'positiveTestsAntibody',
+       'negativeTestsAntibody', 'totalTestsPeopleAntibody',
+       'positiveTestsPeopleAntibody', 'negativeTestsPeopleAntibody',
+       'totalTestsPeopleAntigen', 'positiveTestsPeopleAntigen',
+       'totalTestsAntigen', 'positiveTestsAntigen', 'fips', 
+       'negativeIncrease', 'total', 'totalTestResultsIncrease', 'posNeg',
+        'hash', 'commercialScore',
+       'negativeRegularScore', 'negativeScore', 'positiveScore', 'score',
+       'grade'],1)
 
-#df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+covid_df.insert(covid_df.shape[-1],'fmtDate',pd.to_datetime(covid_df.date, format='%Y%m%d'))
+#covid_df.columns
 
+covid_df.set_index(['state','fmtDate'])
+covid_df.sort_values(by=['state', 'fmtDate'])
+covid_df = covid_df.iloc[::-1]
 
-df.insert(df.shape[-1],'fmtDate',pd.to_datetime(df.date, format='%Y%m%d'))
-#df['fmtDate'] = pd.to_datetime(df.date, format='%Y%m%d')
-#df = df[df.date != 6]
-
-df = df.iloc[::-1]
-
+report_d = {}
+report_df = pd.DataFrame(data=report_d)
+report_df
 
 for index, row in states_df.iterrows():
+    #print(index)
+    #print(row)
+    state = covid_df.loc[covid_df['state'] == row['abbreviation']]
+    loop_df = pd.DataFrame(data=state)
     
+    #print('Loop DF:\n', loop_df)
     
-    state = df.loc[df['state'] == row['abbreviation']]
-    
-    deathIncrease = state['deathIncrease']
-    positiveIncrease = state['positiveIncrease']
+    deathIncrease = loop_df['deathIncrease']
+    positiveIncrease = loop_df['positiveIncrease']
     population = row['population']['estimates']['2019']
     
     divisor = 0
@@ -71,24 +81,23 @@ for index, row in states_df.iterrows():
         divisor = 0
     
     
-    print(row['name'],row['abbreviation'],row['population']['estimates']['2019'], divisor)
-    
-
-    
-
-    if divisor > 0 :
+    #print(row['name'],row['abbreviation'],row['population']['estimates']['2019'], divisor)
+    if divisor > 0:        
         
-        state.insert(state.shape[-1], 'SMA_7_CasesPerPopulation', state.loc[:,'positiveIncrease'].rolling(window=7).mean() / divisor)
-        state.insert(state.shape[-1], 'SMA_28_CasesPerPopulation', state.loc[:,'positiveIncrease'].rolling(window=28).mean() / divisor)
+        loop_df.insert(loop_df.shape[-1], 'positiveIncreasePopulation', loop_df.loc[:,'positiveIncrease'] / divisor)
+        loop_df.insert(loop_df.shape[-1], 'SMA_7_CasesPerPopulation', loop_df.loc[:,'positiveIncrease'].rolling(window=7).mean() / divisor)
+        loop_df.insert(loop_df.shape[-1], 'SMA_28_CasesPerPopulation', loop_df.loc[:,'positiveIncrease'].rolling(window=28).mean() / divisor)
         
-        state.insert(state.shape[-1], 'SMA_7_DeathsPerPopulation', state.loc[:,'deathIncrease'].rolling(window=7).mean() / divisor)
-        state.insert(state.shape[-1], 'SMA_28_DeathsPerPopulation', state.loc[:,'deathIncrease'].rolling(window=28).mean() / divisor)
+        loop_df.insert(loop_df.shape[-1], 'deathIncreasePopulation', loop_df.loc[:,'deathIncrease'] / divisor)
+        loop_df.insert(loop_df.shape[-1], 'SMA_7_DeathsPerPopulation', loop_df.loc[:,'deathIncrease'].rolling(window=7).mean() / divisor)
+        loop_df.insert(loop_df.shape[-1], 'SMA_28_DeathsPerPopulation', loop_df.loc[:,'deathIncrease'].rolling(window=28).mean() / divisor)
+        
         
         fig = plt.figure(figsize=(100,40))
         labelText_cases = row['name'] + " Cases per " + str(divisor) + " people"
-        plt.plot_date(state['fmtDate'], (state['positiveIncrease'] / divisor), color='blue', linestyle='solid', label=labelText_cases)
-        plt.plot_date(state['fmtDate'], (state['SMA_7_CasesPerPopulation']), color='deepskyblue', linestyle='solid', label='7 day rolling average')
-        plt.plot_date(state['fmtDate'], (state['SMA_28_CasesPerPopulation']), color='fuchsia', linestyle='solid', label='28 day rolling average')
+        plt.plot_date(loop_df['fmtDate'], (loop_df['positiveIncrease'] / divisor), color='blue', linestyle='solid', label=labelText_cases)
+        plt.plot_date(loop_df['fmtDate'], (loop_df['SMA_7_CasesPerPopulation']), color='deepskyblue', linestyle='solid', label='7 day rolling average')
+        plt.plot_date(loop_df['fmtDate'], (loop_df['SMA_28_CasesPerPopulation']), color='fuchsia', linestyle='solid', label='28 day rolling average')
         
         plt.grid(b=True, which='both', axis='both')
         plt.legend(loc='upper left')
@@ -98,12 +107,12 @@ for index, row in states_df.iterrows():
         plt.savefig(savePath, dpi=75 )
         #plt.show()
         plt.close()
-
+        
         fig = plt.figure(figsize=(100,40))
         labelText_deaths = row['name'] + " Deaths per " + str(divisor) + " people"
-        plt.plot_date(state['fmtDate'], (state['deathIncrease'] / divisor), color='red', linestyle='solid', label=labelText_deaths)
-        plt.plot_date(state['fmtDate'], (state['SMA_7_DeathsPerPopulation']), color='orange', linestyle='solid', label='7 day rolling average')
-        plt.plot_date(state['fmtDate'], (state['SMA_28_DeathsPerPopulation']), color='brown', linestyle='solid', label='28 day rolling average')
+        plt.plot_date(loop_df['fmtDate'], (loop_df['deathIncrease'] / divisor), color='red', linestyle='solid', label=labelText_deaths)
+        plt.plot_date(loop_df['fmtDate'], (loop_df['SMA_7_DeathsPerPopulation']), color='orange', linestyle='solid', label='7 day rolling average')
+        plt.plot_date(loop_df['fmtDate'], (loop_df['SMA_28_DeathsPerPopulation']), color='brown', linestyle='solid', label='28 day rolling average')
         
         plt.grid(b=True, which='both', axis='both')
         plt.legend(loc='upper left')
@@ -112,20 +121,26 @@ for index, row in states_df.iterrows():
         plt.savefig(savePath, dpi=75 )
         #plt.show()
         plt.close()
+        
+        
+    loop_df.insert(loop_df.shape[-1],'SMA_3_positiveIncrease', loop_df.loc[:,'positiveIncrease'].rolling(window=3).mean())
+    loop_df.insert(loop_df.shape[-1],'SMA_7_positiveIncrease', loop_df.loc[:,'positiveIncrease'].rolling(window=7).mean())
+    loop_df.insert(loop_df.shape[-1],'SMA_28_positiveIncrease', loop_df.loc[:,'positiveIncrease'].rolling(window=28).mean())
+    loop_df.insert(loop_df.shape[-1],'SMA_90_positiveIncrease', loop_df.loc[:,'positiveIncrease'].rolling(window=90).mean())
     
+    loop_df.insert(loop_df.shape[-1],'SMA_3_deathIncrease', loop_df.loc[:,'deathIncrease'].rolling(window=3).mean())
+    loop_df.insert(loop_df.shape[-1],'SMA_7_deathIncrease', loop_df.loc[:,'deathIncrease'].rolling(window=7).mean())
+    loop_df.insert(loop_df.shape[-1],'SMA_28_deathIncrease', loop_df.loc[:,'deathIncrease'].rolling(window=28).mean())
+    loop_df.insert(loop_df.shape[-1],'SMA_90_deathIncrease', loop_df.loc[:,'deathIncrease'].rolling(window=90).mean())
     
-    state.insert(state.shape[-1],'SMA_3_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=3).mean())
-    state.insert(state.shape[-1],'SMA_7_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=7).mean())
-    state.insert(state.shape[-1],'SMA_28_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=28).mean())
-    state.insert(state.shape[-1],'SMA_90_positiveIncrease', state.loc[:,'positiveIncrease'].rolling(window=90).mean())
     
     fig = plt.figure(figsize=(100,40))
     labelText = row['name'] + " Daily Increase in Covid Positive Cases - " + time.strftime("%Y%m%d_%H:%M")
-    plt.plot_date(state['fmtDate'], state['positiveIncrease'], color='black', linestyle='solid', label=labelText)
-    plt.plot_date(state['fmtDate'], state['SMA_3_positiveIncrease'], color='red', linestyle='solid', label='3 day rolling average')
-    plt.plot_date(state['fmtDate'], state['SMA_7_positiveIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
-    plt.plot_date(state['fmtDate'], state['SMA_28_positiveIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
-    plt.plot_date(state['fmtDate'], state['SMA_90_positiveIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['positiveIncrease'], color='black', linestyle='solid', label=labelText)
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_3_positiveIncrease'], color='red', linestyle='solid', label='3 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_7_positiveIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_28_positiveIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_90_positiveIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
     plt.grid(b=True, which='both', axis='both')
     plt.legend(loc='upper left')
     fileName = row['name'].replace(" ","_") + '_CovidGraphPositives' + time.strftime("%Y%m%d") + '.pdf'
@@ -134,19 +149,13 @@ for index, row in states_df.iterrows():
     #plt.show()
     plt.close()
     
-
-    state.insert(state.shape[-1],'SMA_3_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=3).mean())
-    state.insert(state.shape[-1],'SMA_7_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=7).mean())
-    state.insert(state.shape[-1],'SMA_28_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=28).mean())
-    state.insert(state.shape[-1],'SMA_90_deathIncrease', state.loc[:,'deathIncrease'].rolling(window=90).mean())
-    
     fig = plt.figure(figsize=(100,40))
     labelText = row['name'] + " Daily Increase in Covid Deaths - " + time.strftime("%Y%m%d_%H:%M")
-    plt.plot_date(state['fmtDate'], state['deathIncrease'], linestyle='solid', color= 'black', label=labelText)
-    plt.plot_date(state['fmtDate'], state['SMA_3_deathIncrease'], color='red', linestyle='solid', label='3 day rolling average')
-    plt.plot_date(state['fmtDate'], state['SMA_7_deathIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
-    plt.plot_date(state['fmtDate'], state['SMA_28_deathIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
-    plt.plot_date(state['fmtDate'], state['SMA_90_deathIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['deathIncrease'], linestyle='solid', color= 'black', label=labelText)
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_3_deathIncrease'], color='red', linestyle='solid', label='3 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_7_deathIncrease'], color='magenta', linestyle='solid', label='7 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_28_deathIncrease'], color='orange', linestyle='solid', label='28 day rolling average')
+    plt.plot_date(loop_df['fmtDate'], loop_df['SMA_90_deathIncrease'], color='navy', linestyle='solid', label='90 day rolling average')
     plt.grid(b=True, which='both', axis='both')
     plt.legend(loc='upper left')
     fileName = row['name'].replace(" ","_") + '_CovidGraphDeaths' + time.strftime("%Y%m%d") + '.pdf'
@@ -155,3 +164,5 @@ for index, row in states_df.iterrows():
     #plt.show()
     plt.close()
     
+    report_df = report_df.append(loop_df, ignore_index=True, sort=False)
+    loop_df = pd.DataFrame(data={})
